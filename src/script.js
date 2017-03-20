fd750aa2241544aaa92af510800ae38480765e3c
Aaron Magid <ahm64@case.edu>
2017-03-20 14:37:30 -0400

    feat(optionalWork): Fixed closure issue on query output.

--- src/script.js
+++ src/script.js
@@ -1,37 +1,43 @@
 $(document).ready(function() {
     $('#submit_custom').click(function() {
+        var elem = $(this);
         $.post('/custom', { query: $(this).sibings('.custom_query').val() }, function(result) {
-            $(this).siblings('.output').text(result);
+            elem.siblings('.output').text(result);
         });
     });
 
     $('#submit_1').click(function() {
+        var elem = $(this);
         $.get('/q1', { starName: $(this).siblings('.query').val() }, function(result) {
-            $(this).siblings('.output').text(result);
+            elem.siblings('.output').text(result);
         });
     });
     
     $('#submit_2').click(function() {
+        var elem = $(this);
         $.get('/q2', {}, function(result) {
-            $(this).siblings('.output').text(result);
+            elem.siblings('.output').text(result);
         });
     });
     
     $('#submit_3').click(function() {
+        var elem = $(this);
         $.get('/q3', { studioName: $(this).siblings('.query').val() }, function(result) {
-            $(this).siblings('.output').text(result);
+            elem.siblings('.output').text(result);
         });
     });
     
     $('#submit_4').click(function() {
+        var elem = $(this);
         $.get('/q4', {}, function(result) {
-            $(this).siblings('.output').text(result);
+            elem.siblings('.output').text(result);
         });
     });
     
     $('#submit_5').click(function() {
+        var elem = $(this);
         $.get('/q5', {}, function(result) {
-            $(this).siblings('.output').text(result);
+            elem.siblings('.output').text(result);
         });
     });
 });
