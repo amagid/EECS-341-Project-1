6331a659a2ebe0e38aa02550872caf1d18327d6b
Aaron Magid <ahm64@case.edu>
2017-03-20 14:37:50 -0400

    feat(optionalWork): JSON.stringify results before printing them out.

--- src/script.js
+++ src/script.js
@@ -2,42 +2,42 @@ $(document).ready(function() {
     $('#submit_custom').click(function() {
         var elem = $(this);
         $.post('/custom', { query: $(this).sibings('.custom_query').val() }, function(result) {
-            elem.siblings('.output').text(result);
+            elem.siblings('.output').text(JSON.stringify(result));
         });
     });
 
     $('#submit_1').click(function() {
         var elem = $(this);
         $.get('/q1', { starName: $(this).siblings('.query').val() }, function(result) {
-            elem.siblings('.output').text(result);
+            elem.siblings('.output').text(JSON.stringify(result));
         });
     });
     
     $('#submit_2').click(function() {
         var elem = $(this);
         $.get('/q2', {}, function(result) {
-            elem.siblings('.output').text(result);
+            elem.siblings('.output').text(JSON.stringify(result));
         });
     });
     
     $('#submit_3').click(function() {
         var elem = $(this);
         $.get('/q3', { studioName: $(this).siblings('.query').val() }, function(result) {
-            elem.siblings('.output').text(result);
+            elem.siblings('.output').text(JSON.stringify(result));
         });
     });
     
     $('#submit_4').click(function() {
         var elem = $(this);
         $.get('/q4', {}, function(result) {
-            elem.siblings('.output').text(result);
+            elem.siblings('.output').text(JSON.stringify(result));
         });
     });
     
     $('#submit_5').click(function() {
         var elem = $(this);
         $.get('/q5', {}, function(result) {
-            elem.siblings('.output').text(result);
+            elem.siblings('.output').text(JSON.stringify(result));
         });
     });
 });
