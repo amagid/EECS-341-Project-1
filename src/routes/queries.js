5714a7d13e1d5ecaee70187f34323953f75b058c
Aaron Magid <ahm64@case.edu>
2017-03-20 01:58:01 -0400

    feat(routes): Added skeletons of query functions.

---
+++ src/routes/queries.js
@@ -0,0 +1,34 @@
+//This file simply contains a bunch of querying functions, both static and dynamic. Exists only for organization.
+
+module.exports = {
+    q1,
+    q2,
+    q3,
+    q4,
+    q5
+};
+
+//Q1: Titles and Years of movies with a particular Star
+function q1(starName) {
+
+}
+
+//Q2: Titles and Years of longest movies for each Studio
+function q2() {
+
+}
+
+//Q3: Name of richest movie producer for a given Studio
+function q3(studioName) {
+
+}
+
+//Q4: Names of movie stars who star only in movies made by studios with same address
+function q4() {
+
+}
+
+//Q5: Names of movie stars who starred in all movies produced by MGM studios... not a dynamic query?
+function q5() {
+
+}
