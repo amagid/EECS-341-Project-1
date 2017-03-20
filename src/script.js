734e641886932aea93b892cb172740f01f075caf
Aaron Magid <ahm64@case.edu>
2017-03-20 14:32:25 -0400

    feat(optionalWork): Added other frontend handlers.

--- src/script.js
+++ src/script.js
@@ -6,22 +6,32 @@ $(document).ready(function() {
     });
 
     $('#submit_1').click(function() {
-        
+        $.get('/q1', { starName: $(this).siblings('.query').val() }, function(result) {
+            $(this).siblings('.output').text(result);
+        });
     });
     
     $('#submit_2').click(function() {
-
+        $.get('/q2', {}, function(result) {
+            $(this).siblings('.output').text(result);
+        });
     });
     
     $('#submit_3').click(function() {
-
+        $.get('/q3', { studioName: $(this).siblings('.query').val() }, function(result) {
+            $(this).siblings('.output').text(result);
+        });
     });
     
     $('#submit_4').click(function() {
-
+        $.get('/q4', {}, function(result) {
+            $(this).siblings('.output').text(result);
+        });
     });
     
     $('#submit_5').click(function() {
-
+        $.get('/q5', {}, function(result) {
+            $(this).siblings('.output').text(result);
+        });
     });
 });
