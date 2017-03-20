6dd055014808cbccd54fde04de619124103d8944
Aaron Magid <ahm64@case.edu>
2017-03-20 14:29:44 -0400

    feat(optionalWork): Added handler function for custom query.

--- src/script.js
+++ src/script.js
@@ -1,3 +1,27 @@
 $(document).ready(function() {
-    console.log('ready');
+    $('#submit_custom').click(function() {
+        $.post('/custom', { query: $(this).sibings('.custom_query').val() }, function(result) {
+            $(this).siblings('.output').text(result);
+        });
+    });
+
+    $('#submit_1').click(function() {
+        
+    });
+    
+    $('#submit_2').click(function() {
+
+    });
+    
+    $('#submit_3').click(function() {
+
+    });
+    
+    $('#submit_4').click(function() {
+
+    });
+    
+    $('#submit_5').click(function() {
+
+    });
 });
